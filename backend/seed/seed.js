7fea2118ce2cc3614f2c28257a2e48916eef0b90
SUCHISMITA SINGHA ROY <suchismitasingharoy@SUCHISMITAs-MacBook-Air.local>
2025-02-05 05:34:58 +0530

    seed updated

--- backend/seed/seed.js
+++ backend/seed/seed.js
@@ -1,34 +1,19 @@
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { faker } from "@faker-js/faker";
-dotenv.config({ path: ".env" });
+import bcrypt from "bcrypt";
 
 
 import userSchema from "../models/userSchema.js";
 import noteSchema from "../models/noteSchema.js";
-import bcrypt from "bcrypt";
+import dbConnect from "../config/dbConnection.js";
 
-const url = process.env.URL
-console.log(url)
-
-
-async function dbConnect() {
-    await mongoose.connect(url)
-        .then(() => {
-            console.log("Mongodb connected");
-        }).
-        catch((Error) => {
-            console.log(Error);
-        })
-}
-dbConnect()
+dotenv.config({ path: ".env" });
 
 const generateUsers = async (num) => {
     const newUsers = [];
     for (let i = 1; i <= num; i++) {
-        let password = "password";
-        const hash = await bcrypt.hash(password, 10)
-        password = hash
+        const password = await bcrypt.hash("password", 10);
         const email = faker.internet.email();
         const fname = faker.internet.username();
         const verified = true;
@@ -37,37 +22,40 @@ const generateUsers = async (num) => {
     return newUsers;
 };
 
-
 const generateNotes = (num) => {
     const newNotes = [];
     for (let i = 1; i <= num; i++) {
-        const title = faker.internet.password();
-        const content = faker.internet.email();
-        const userID = "67a0c3600de5516b3cc2efa6"
+        const title = faker.lorem.words(3);
+        const content = faker.lorem.sentence();
+        const userID = "67a2a6413018642200432bd1";
         newNotes.push({ title, content, userID });
     }
     return newNotes;
 };
 
-const newNotes = generateNotes(10);
+const seedDatabase = async () => {
+    try {
+        await dbConnect(); 
+        console.log("Connected to Database");
 
-const seedUser = async () => {
-    const newUsers = await generateUsers(10);
-    await userSchema.deleteMany({});
-    await userSchema.insertMany(newUsers);
+        // Seed Users
+        const newUsers = await generateUsers(10);
+        // await userSchema.deleteMany({});
+        await userSchema.insertMany(newUsers);
+        console.log("Successfully Created Users");
+
+        // Seed Notes
+        const newNotes = generateNotes(b);
+        await noteSchema.deleteMany({});
+        await noteSchema.insertMany(newNotes);
+        console.log("Successfully Created Notes");
+
+    } catch (error) {
+        console.error("Seeding Error:", error);
+    } finally {
+        mongoose.disconnect();
+        console.log("Database Disconnected");
+    }
 };
 
-const seedNotes = async () => {
-    await noteSchema.deleteMany({});
-    await noteSchema.insertMany(newNotes);
-};
-
-seedUser().then(() => {
-    console.log("Successfully Created Users");
-    mongoose.disconnect()
-});
-
-seedNotes().then(() => {
-    console.log("Successfully Created Notes");
-    mongoose.disconnect()
-});
+seedDatabase();
